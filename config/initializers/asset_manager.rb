db3f0090e8c5594efbfc282018f98d0e8c4b6afb
Chris Lowis <chris.lowis@gmail.com>
2017-10-05 12:01:46 +0100

    Add a directory for temporary storage of assets
    This directory is used for the temporary storage of assets before they
    are uploaded to asset manager. It will be added to the disks in all
    environments using puppet.

--- config/initializers/asset_manager.rb
+++ config/initializers/asset_manager.rb
@@ -1,1 +1,7 @@
 Whitehall.use_asset_manager = ENV['USE_ASSET_MANAGER'].present?
+
+module Whitehall
+  def self.asset_manager_tmp_dir
+    File.join(Whitehall.uploads_root, 'asset-manager-tmp')
+  end
+end
